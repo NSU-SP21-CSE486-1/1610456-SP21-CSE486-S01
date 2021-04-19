bd98c06e61084bdd17b3c2a7b8da8ea84d387aff
sadmanhossainridoy <tunrjor>
2021-04-19 08:15:56 +0600

    Student signup, login, Management signup, login commited

---
+++ Lab/Lab04/app/src/main/java/com/cse486/lab04/Calculator.java
@@ -0,0 +1,25 @@
+package com.cse486.lab04;
+
+
+public class Calculator {
+
+    // Available operations
+    public enum Operator {ADD, SUB, DIV, MUL}
+
+
+    public double add(double firstOperand, double secondOperand) {
+        return firstOperand + secondOperand;
+    }
+
+    public double sub(double firstOperand, double secondOperand) {
+        return firstOperand - secondOperand;
+    }
+
+    public double div(double firstOperand, double secondOperand) {
+        return firstOperand / secondOperand;
+    }
+
+    public double mul(double firstOperand, double secondOperand) {
+        return firstOperand * secondOperand;
+    }
+}
